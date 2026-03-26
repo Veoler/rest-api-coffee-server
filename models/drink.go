953e18d2b442b99185ad113a5426256a57732189
Veoler <iiterhoev@gmail.com>
2026-03-26 12:12:37 +0300

    Релиз 2: Структура напитка готова

---
+++ models/drink.go
@@ -0,0 +1,11 @@
+package drink
+
+type Drink struct {
+	ID				int 	`json:"id"`
+	Name			string 	`json:"name"`
+	Price			int 	`json:"price"`
+	InStock			bool 	`json:"inStock"`
+	IsCaffeine		bool 	`json:"isCaffeine"`
+	Volume			int 	`json:"volume"`
+	Description		string 	`json:"description"`
+}
